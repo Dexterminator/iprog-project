25560d0d240389e7abbdccd1857fb2014c75d667
Ludvig Jansson <ludjan@kth.se>
2014-03-08 08:43:20 -0500

    Quick hotfix with an ID change and instantiation of formView

--- js/view/formView.js
+++ js/view/formView.js
@@ -1,5 +1,11 @@
 // formView object constructor
 var FormView = function (container, model){
+	var formInputName = container.find("formInputName");
+	var formInputLength = container.find("formInputLength");
+	var formInputType = container.find("formInputType");
+	var formInputDesc = container.find("formInputDesc");
+	var formSubmitCancel = container.find("formSubmitCancel");
+	var formSubmitSave = container.find("formSubmitSave");
 	//Load objects from view
 	//No need for observer pattern
 
