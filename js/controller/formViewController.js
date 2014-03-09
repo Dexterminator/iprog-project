c116c48de42a3fa5f8fa74628aaf65e1068a0c6e
Dexter Gramfors <dexter.gramfors@gmail.com>
2014-03-09 16:59:58 +0100

    Cleared the form after save or cancel.

--- js/controller/formViewController.js
+++ js/controller/formViewController.js
@@ -2,6 +2,9 @@
 var FormViewController = function (view, model){
 	view.formSubmitSave.click(function() {
 		var act = new Activity(view.formInputName.val(),view.formInputLength.val(),view.formInputType.val(),view.formInputDesc.val());
+		view.formInputName.val("");
+		view.formInputLength.val("");
+		view.formInputDesc.val("");
 		model.addParkedActivity(act);
 		window.showAgendas();
 	});
